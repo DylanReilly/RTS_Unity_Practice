c8f032558fc6bb6345dbcab67e1d02a31d05a714
Dylan Reilly <dylanreilly81@gmail.com>
2021-02-04 12:29:20 +0000

    Fixed health display issue
    Rebuilt both the building and unit base prefabs. Issue was cause by both the prefab and the building model having a box collider. Removing the building box collider fixed the issue

---
+++ Assets/Scripts/MouseTest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseTest : MonoBehaviour
+{
+    private void OnMouseEnter()
+    {
+        Debug.Log("This is working");
+    }
+}
